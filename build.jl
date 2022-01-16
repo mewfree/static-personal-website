7ad9a647ee13e4aca582b73d48777582e40f0d2c
Damien Gonot <damien.gonot@gmail.com>
2022-01-16 18:19:22 -0500

    add meta description

--- build.jl
+++ build.jl
@@ -29,13 +29,14 @@ for filename in readdir("src/posts")
     else
     end
     content = read(`pandoc src/posts/$filename --lua-filter filter.lua`, String)
+    excerpt = read(`pandoc src/posts/$filename -t plain`, String)
 
     post = Dict("slug" => slug, "title" => title, "date" => date, "tags" => tags)
     push!(posts, post)
 
     template = read("src/templates/post.html", String)
     templated_string = replace(template, "{TITLE}" => title, "{DATE}" => date, "{CONTENT}" => content)
-    output = replace(header, "{TITLE}" => title * " - Damien Gonot") * templated_string * footer
+    output = replace(header, "{TITLE}" => title * " - Damien Gonot", "{DESCRIPTION}" => "$(first(excerpt, 297))...") * templated_string * footer
     open("build/blog/$slug.html", "w") do io
         write(io, output)
     end
@@ -47,21 +48,19 @@ post_link_template = read("src/templates/post-link.html", String)
 posts_template = read("src/templates/posts.html", String)
 posts_list = [replace(post_link_template, "{TITLE}" => post["title"], "{SLUG}" => post["slug"], "{DATE}" => post["date"]) for post = posts]
 content = replace(posts_template, "{CONTENT}" => join(posts_list))
-output = replace(header, "{TITLE}" => "Blog Posts - Damien Gonot") * content * footer
+output = replace(header, "{TITLE}" => "Blog Posts - Damien Gonot", "{DESCRIPTION}" => "List of blog posts by Damien Gonot.") * content * footer
 open("build/blog.html", "w") do io
     write(io, output)
 end
 
 # Other routes
 routes = [
-    Dict("destination" => "index.html", "source" => "index.html", "title" => "Damien Gonot"),
-    Dict("destination" => "about.html", "source" => "about.org", "title" => "About - Damien Gonot"),
+    (destination = "index.html", source = "index.html", title = "Damien Gonot", description = "Homepage of Damien Gonot's personal website."),
+    (destination = "about.html", source = "about.org", title = "About - Damien Gonot", description = "Learn more about Damien Gonot."),
 ]
 
 for route in routes
-    destination = route["destination"]
-    source = route["source"]
-    title = route["title"]
+    (; destination, source, title, description) = route
 
     if endswith(source, ".org")
         local content = read(`pandoc src/$source`, String)
@@ -74,7 +73,7 @@ for route in routes
         local content = replace(content, "{RECENT_BLOG_POSTS}" => join(posts_list[1:5]))
     end
 
-    local output = replace(header, "{TITLE}" => title) * content * footer
+    local output = replace(header, "{TITLE}" => title, "{DESCRIPTION}" => description) * content * footer
     open("build/$destination", "w") do io
         write(io, output)
     end
